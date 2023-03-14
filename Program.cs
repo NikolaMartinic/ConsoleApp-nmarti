1d11e7b388b20d5da27a7ce9f3be09c86e36b8b7
nmarti00 <nmarti00@fesb.hr>
2023-03-14 16:17:48 +0100

    Hello

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp_nmarti
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello !");
         }
     }
 }
